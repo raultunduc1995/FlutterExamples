44be5ca65d7feff8b791c95e2b4ca477fd567374
raultunduc1995 <raultunduc1995@gmail.com>
2021-02-24 21:23:37 +0200

    Implement infinite scrolling startup generator

--- FlutterExample1/flutter_example_one/lib/main.dart
+++ FlutterExample1/flutter_example_one/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -75,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(
+            child: Text(widget.title)
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -101,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have clicked the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$_counter time(s)',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
